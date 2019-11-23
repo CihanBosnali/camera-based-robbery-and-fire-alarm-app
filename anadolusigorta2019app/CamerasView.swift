1631e85caaffe4c4a046673f537da78c1f9bfdfe
AydaSelen <aselen22@my.sevkoleji.k12.tr>
2019-11-23 16:36:44 +0300

    kilitleme

--- anadolusigorta2019app/CamerasView.swift
+++ anadolusigorta2019app/CamerasView.swift
@@ -12,7 +12,7 @@ struct CamerasView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-                CameraListRow(videoAvailable: false, name: "Oturma Odası")
+                CameraListRow(videoAvailable: false, cameraAvailable: false, name: "Oturma Odası")
             }.navigationBarTitle(Text("Evim > Kameralar"))
 
         }
