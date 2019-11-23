dc28a5793c43525a898fc3ab00a1fcf94d5c75e4
Cihan Bosnalı <cihanbosna@hotmail.com>
2019-11-23 14:53:50 +0300

    a

--- anadolusigorta2019app/HomeView.swift
+++ anadolusigorta2019app/HomeView.swift
@@ -9,10 +9,23 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    init() {
+        UINavigationBar.appearance().backgroundColor = UIColor(red: 48.0/255.0, green: 31.0/255.0, blue: 92.0/255.0, alpha:1.0)
+    
+        UINavigationBar.appearance().largeTitleTextAttributes = [
+            .foregroundColor: UIColor.white]
+    }
+
     var body: some View {
-        Text("HomeView!")
+        NavigationView{
+            Text("a")
+                .navigationBarTitle(
+                    Text("Evim"))
+        }
     }
 }
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
