c61a07d30f6ed8efbf244db8874b0d23a23fcbdd
Cihan Bosnalı <cihanbosna@hotmail.com>
2019-11-23 11:25:41 +0300

    Initial Commit

---
+++ anadolusigorta2019app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  anadolusigorta2019app
+//
+//  Created by cihan on 23.11.2019.
+//  Copyright © 2019 Cihan Bosnalı. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
