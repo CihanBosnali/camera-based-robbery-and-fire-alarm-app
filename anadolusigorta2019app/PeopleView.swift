d6a40f6461bd1ca334f5c79dbffa5eb987181fbd
AydaSelen <aselen22@my.sevkoleji.k12.tr>
2019-11-24 09:50:28 +0300

    çalışıyor

--- anadolusigorta2019app/PeopleView.swift
+++ anadolusigorta2019app/PeopleView.swift
@@ -12,11 +12,18 @@ struct PeopleView: View {
     var body: some View {
         NavigationView {
             List {
-                NavigationLink(destination: /*@START_MENU_TOKEN@*/ /*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                NavigationLink(destination: /*@START_MENU_TOKEN@*/ /*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/){
                     PersonRow(name: "Ayda Selen Bozkurt", email: "aselen@gmail.com", number: "0599 999 9999")
                 }
                 NavigationLink(destination: /*@START_MENU_TOKEN@*/ /*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
                     PersonRow(name: "Cihan Alperen Bosnalı", email: "cbosnali@gmail.com", number: "0533 333 3333")
+                
+                    
+                }
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image("Kisiekle").renderingMode(Image.TemplateRenderingMode?.init(Image.TemplateRenderingMode.original))
+        
+                    
                 }
             }.navigationBarTitle(Text("Evim > Kişiler"))
         }
@@ -28,3 +35,4 @@ struct PeopleView_Previews: PreviewProvider {
         PeopleView()
     }
 }
+
